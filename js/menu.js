5f86e2da67b96eff87a30f3d18e7da78cc89a9bc
Unknown <reiza.gabriel21@gmail.com>
2018-04-02 22:39:15 -0700

    New menu

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,9 @@
-/* Open when someone clicks on the span element */
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-}
-
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " overlay";
+    } else {
+        x.className = "topnav";
+    }
 }
